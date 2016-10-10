a830b705e003d00bd437f1f416f5adc4df94c0bd
Erik <Erik@Erik-PC.Home>
2016-10-09 23:24:29 -0300

    criacao da classe de conexao e tambem da generica

---
+++ Sistema_DevGo/src/main/java/sistema/devgo/Model/dao/GenericaDAO.java
@@ -0,0 +1,63 @@
+package sistema.devgo.Model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Erik
+ */
+public class GenericaDAO {
+    
+
+
+public abstract class GenericaDao {
+    private final Connection connection;
+
+    protected GenericaDao() {
+        this.connection = ConexaoBD.getConnection();
+    }
+
+    protected Connection getConnection() {
+        return connection;
+    }
+
+    protected void save(String insertSql, Object... parametros) throws SQLException {
+        PreparedStatement pstmt = 
+			getConnection().prepareStatement(insertSql);
+
+        for (int i = 0; i < parametros.length; i++) {
+            pstmt.setObject(i+1, parametros[i]);
+        }
+
+        pstmt.execute();
+        pstmt.close();
+    }
+
+    protected void update(String updateSql, Object id, Object... parametros) throws SQLException {
+        PreparedStatement pstmt = 
+			getConnection().prepareStatement(updateSql);
+			
+        for (int i = 0; i < parametros.length; i++) {
+            pstmt.setObject(i+1, parametros[i]);
+        }
+        pstmt.setObject(parametros.length + 1, id);
+        pstmt.execute();
+        pstmt.close();
+    }
+
+    protected void delete(String deleteSql, Object... parametros) throws SQLException {
+        PreparedStatement pstmt = 
+			getConnection().prepareStatement(deleteSql);
+			
+        for (int i = 0; i < parametros.length; i++) {
+            pstmt.setObject(i+1, parametros[i]);
+        }
+
+        pstmt.execute();
+        pstmt.close();
+    }
+ }
+    
+}
