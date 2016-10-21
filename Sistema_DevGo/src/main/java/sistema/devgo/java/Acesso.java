87c60a153ce5c15aab821cab179e0aaa7bfe5b17
erik.omoreira <erik.omoreira@CAS555101W0728.senacsp.edu.br>
2016-10-21 21:43:20 -0200

    atualização de classes

--- Sistema_DevGo/src/main/java/sistema/devgo/java/Acesso.java
+++ Sistema_DevGo/src/main/java/sistema/devgo/java/Acesso.java
@@ -5,18 +5,62 @@ package sistema.devgo.java;
  * @author Sibele
  */
 public class Acesso {
+    
     private String usuario;
     private String senha;
-    //Associando acesso a funcionario
     private Funcionario Funcionario;
+   
+      
+     public Acesso(String usuario, String senha){
+        
+         this.usuario = usuario;
+         this.senha = senha;
+        
+    }
 
-    
-    /*
-    public ? TipoPermissao( String usuario, String senha){
-      //VERIFICAR COMO FAZER   
-    }*/
-    
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the Funcionario
+     */
     public Funcionario getFuncionario() {
         return Funcionario;
     }
+
+    /**
+     * @param Funcionario the Funcionario to set
+     */
+    public void setFuncionario(Funcionario Funcionario) {
+        this.Funcionario = Funcionario;
+    }
+    
+      
+    
+    
 }
