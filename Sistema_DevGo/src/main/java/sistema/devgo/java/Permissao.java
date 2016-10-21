f08ee2e09e5662d235d4a3aa6240dab6b2fdc370
natanael.ssousa <natanael.ssousa@CAS555103W0726.senacsp.edu.br>
2016-10-21 21:30:45 -0200

    Funcionario, Permissao

--- Sistema_DevGo/src/main/java/sistema/devgo/java/Permissao.java
+++ Sistema_DevGo/src/main/java/sistema/devgo/java/Permissao.java
@@ -1,4 +1,3 @@
-
 package sistema.devgo.java;
 
 /**
@@ -6,9 +5,36 @@ package sistema.devgo.java;
  * @author Sibele
  */
 public class Permissao {
+
     private String departamento;
-    
-    public void nivelPermissao(String departamento){
+
+    public void nivelPermissao(String departamento) {
         // 
+    }
+//
+    public int validaPermissao(Permissao tipoPermissao) {
+
+        if ("ti".equals(tipoPermissao.getDepartamento())) {
+            return 1;
+        } else if ("financeiro".equals(tipoPermissao)) {
+            return 2;
+        } else if ("serviços".equals(tipoPermissao)) {
+            return 3;
+        }
+        return 0;
+    }
+
+    /**
+     * @return the departamento
+     */
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    /**
+     * @param departamento the departamento to set
+     */
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
     }
 }
