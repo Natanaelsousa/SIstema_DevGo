b7d8bbfe41d26e44a128a20eb7fe3e1f1c7d791b
SibeleNepomuceno <sibele_nep@outlook.com>
2016-10-29 21:13:35 -0200

    correção da classe clienteDAO

--- Sistema_DevGo/src/main/java/sistema/devgo/Model/dao/ClienteDAO.java
+++ Sistema_DevGo/src/main/java/sistema/devgo/Model/dao/ClienteDAO.java
@@ -10,35 +10,8 @@ import sistema.devgo.java.Cliente;
  * @author Sibele
  */
 
-public class ClienteDAO {
-    public ClienteDAO(){
-        
-    }
-    public void CadastrarCliente(Cliente cliente){
-        try {
-            String sql ="insert into CLIENTE(CNPJ, RAZAO_SOCIAL, CIDADE, TELEFONE1, "
-                    + " TELEFONE2, RUA, EMAIL, ESTADO, NUMERO, BAIRRO, CEP) values(?, ?, ?, ?, ?, ?,"
-                    + " ?, ?, ?, ?, ?);";
-            PreparedStatement stm= ConexaoBD.getConnection().prepareStatement(sql);
-            stm.setString(1, cliente.getCNPJ());
-            stm.setString(2,  cliente.getRazaoSocial());
-            stm.setString(3,  cliente.getCidade());
-            stm.setString(4,  cliente.getTelefone1());
-            stm.setString(5,  cliente.getTelefone2());
-            stm.setString(6,  cliente.getRua());
-            stm.setString(7,  cliente.getEmail());
-            stm.setString(8,  cliente.getEstado());
-            stm.setString(9,  cliente.getNumero());
-            stm.setString(10,  cliente.getBairro());
-            stm.setString(11,  cliente.getCep());
-            
-            stm.execute();
-            
-        } catch (SQLException e) {
-            System.out.println("Erro ao cadastrar cliente" +e.getMessage());
-                
-        }
-    }
+public class ClienteDAO extends GenericaDAO {
 
+    
     
 }
