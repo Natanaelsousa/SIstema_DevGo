ae37924c65c08ab2d80556f98cea80b4ae094ef4
erik.omoreira <erik.omoreira@CAS485675W07C01.senacsp.edu.br>
2016-11-05 14:56:32 -0200

    alteracao conexao

--- Sistema_DevGo/src/main/java/sistema/devgo/Model/dao/ConexaoBD.java
+++ Sistema_DevGo/src/main/java/sistema/devgo/Model/dao/ConexaoBD.java
@@ -13,12 +13,12 @@ import java.sql.SQLException;
  */
 public class ConexaoBD {
 
-    private static final String SERVIDOR = "localhost";//caminho do servidor do BD
+    private static final String SERVIDOR = "10.135.22.73";//caminho do servidor do BD
     private static final String BANCO = "db_curso"; //nome do banco de dados
     private static final String URL = "jdbc:mysql://" + SERVIDOR + "/" + BANCO; //url de conexao
     private static final String DRIVER = "com.mysql.jdbc.Driver"; //driver mysql
-    private static final String USUARIO = "root"; //nome do usuário do banco de dados
-    private static final String SENHA = "beli1771"; //senha do usuário do banco de dados
+    private static final String USUARIO = "natanael"; //nome do usuário do banco de dados
+    private static final String SENHA = ""; //senha do usuário do banco de dados
 
     public static Connection getConnection() {
 
