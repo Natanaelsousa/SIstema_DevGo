8968488f18f85f93d8054895f42b110c7e36b405
Natanael Santos <>
2016-11-19 23:37:40 -0200

    Metodo para somar campos em vendas com JS, falta implementar na tela de vendas

--- Sistema_DevGo/src/main/webapp/SCRIPT/funcoes.js
+++ Sistema_DevGo/src/main/webapp/SCRIPT/funcoes.js
@@ -167,3 +167,15 @@ function validarCNPJ(cnpj) {
     return true;
 
 }
+function calcular() {
+    var num1 = Number(document.getElementById("Aluno").value);
+    var num2 = Number(document.getElementById("num2").value);
+    var elemResult = document.getElementById("resultado");
+
+    if (elemResult.textContent === undefined) {
+       elemResult.textContent = "O resultado é " + String(num1 + num2) + ".";
+    }
+    else { // IE
+       elemResult.innerText = "O resultado é " + String(num1 + num2) + ".";
+    }
+}
